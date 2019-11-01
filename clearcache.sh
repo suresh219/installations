cd6e7bd02f863254f698e58ad922454ff114ced5
suresh <asureshvkd@gmail.com>
2019-11-01 09:04:24 +0000

    memory clean

---
+++ clearcache.sh
@@ -0,0 +1,8 @@
+#Clear PageCache only
+sync; echo 1 > /proc/sys/vm/drop_caches
+
+#Clear dentries and inodes
+sync; echo 2 > /proc/sys/vm/drop_caches
+
+# Clear PageCache, dentries and inodes.
+sync; echo 3 > /proc/sys/vm/drop_caches 
